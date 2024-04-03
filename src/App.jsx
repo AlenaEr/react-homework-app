e5f874f122f86d241637eef940daf1ba2c9c574c
alenaer <alana.eremina@gmail.com>
2024-04-03 18:01:46 +0200

    homework_43

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Form from './components/Form';
+import Menu from './components/Menu';
 import './css/index.css';
 import './css/login.css';
+import './css/menu.css';
 
 const App = () => {
   return (
@@ -12,9 +15,14 @@ const App = () => {
         <h1 className="title">The best pizza.<br /><span className="text-yellow">Straight out of the oven, straight to you.</span></h1>
         <p className="sub-title">👋 Welcome! Please start by telling us your name:</p>
         <Form />
+        <Menu />
       </main>
     </div>
   );
 };
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
 
 export default App;
