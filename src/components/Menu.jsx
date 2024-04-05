fdde5465e2532e41a8b3a22b94d680f17f528889
alenaer <alana.eremina@gmail.com>
2024-04-05 19:13:57 +0200

    homework_44

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 import "../css/menu.css";
 import { pizzas } from "../menu/data";
 
-const PizzaItem = ({ name, ingredients, unitPrice, soldOut, imageUrl }) => (
-  <li className="pizza">
-    <img src={imageUrl} alt={name} className="pizza__image" />
-    <div className="pizza__info">
-      <p className="pizza__name">{name}</p>
-      <p className="pizza__ingredients">{ingredients.join(", ")}</p>
-      <div className="pizza__actions">
-        {soldOut ? (
-          <p className="pizza__price">Sold out</p>
-        ) : (
-          <>
-            <p className="pizza__price">€{unitPrice.toFixed(2)}</p>
-            <button className="button">Add to cart</button>
-          </>
-        )}
+const PizzaItem = ({ name, ingredients, unitPrice, soldOut, imageUrl }) => {
+  const [quantity, setQuantity] = useState(0);
+  const handleAdd = () => {
+    setQuantity(quantity + 1);
+  };
+  const handleDelete = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+  return (
+    <li className="pizza">
+      <img src={imageUrl} alt={name} className="pizza__image" />
+      <div className="pizza__info">
+        <p className="pizza__name">{name}</p>
+        <p className="pizza__ingredients">{ingredients.join(", ")}</p>
+        <div className="pizza__actions">
+          {soldOut ? (
+            <p className="pizza__price">Sold out</p>
+          ) : (
+            <>
+              <p className="pizza__price">€{unitPrice.toFixed(2)}</p>
+              <div>
+              {/* <button className="button">Delete</button> */}
+                <button onClick={handleDelete} className="button">
+                  -
+                </button>
+                <span>{quantity}</span>
+                <button onClick={handleAdd} className="button">
+                  +
+                </button>
+                {/* <button className="button">Add to cart</button> */}
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 const Menu = () => (
   <ul>
