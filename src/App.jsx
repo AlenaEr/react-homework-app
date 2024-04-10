a48f7acddaf27a4e2ca53b6a6cc5a909f0bb9b67
alenaer <alana.eremina@gmail.com>
2024-04-10 21:28:22 +0200

    homework_47

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,49 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import Menu from "./components/Menu";
+import { UserNameProvider } from "./contexts/UserNameProvider";
 import "./css/index.css";
 import "./css/login.css";
 import "./css/menu.css";
 
 const App = () => {
   return (
-    <Router>
-      <div className="wrapper">
-        <Header />
-        <main className="content">
-          <Routes>
-            <Route path="/login" element={<h1>Login Page</h1>} />
-            <Route path="/menu" element={<h1>Menu Page</h1>} />
-            <Route
-              path="/"
-              element={
-                <>
-                  <h1 className="title">
-                    The best pizza.
-                    <br />
-                    <span className="text-yellow">
-                      Straight out of the oven, straight to you.
-                    </span>
-                  </h1>
-                  <p className="sub-title">
-                    👋 Welcome! Please start by telling us your name:
-                  </p>
-                  <Form />
-                  <Menu />
-                </>
-              }
-            />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <UserNameProvider>
+      <Router>
+        <div className="wrapper">
+          <Header />
+          <main className="content">
+            <Routes>
+              <Route path="/login" element={<h1>Login Page</h1>} />
+              <Route path="/menu" element={<h1>Menu Page</h1>} />
+              <Route
+                path="/"
+                element={
+                  <>
+                    <h1 className="title">
+                      The best pizza.
+                      <br />
+                      <span className="text-yellow">
+                        Straight out of the oven, straight to you.
+                      </span>
+                    </h1>
+                    <p className="sub-title">
+                      👋 Welcome! Please start by telling us your name:
+                    </p>
+                    <Form />
+                    <Menu />
+                  </>
+                }
+              />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </UserNameProvider>
   );
 };
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 export default App;
